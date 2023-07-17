642c4c4d708f6f2f79b45935a9c16a01649b70eb
JordanAlec <Jordanwhittaker2704@outlook.com>
2023-07-17 16:41:19 +0100

    Updated reading events to handle partial batch reading

--- src/MinimalWebHooks.Core/Processors/WebhookActionEventProcessor.cs
+++ src/MinimalWebHooks.Core/Processors/WebhookActionEventProcessor.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Channels;
+﻿using System.Reflection.PortableExecutable;
+using System.Threading;
+using System.Threading.Channels;
+using MinimalWebHooks.Core.Extensions;
 using MinimalWebHooks.Core.Interfaces;
 using MinimalWebHooks.Core.Models;
 
@@ -6,10 +9,12 @@ namespace MinimalWebHooks.Core.Processors;
 
 public class WebhookActionEventProcessor : IWebhookActionEventProcessor
 {
+    private readonly int _maxCapacity;
     private readonly Channel<WebhookActionEvent> _webhookActionEventChannel;
 
     public WebhookActionEventProcessor(WebhookOptions options)
     {
+        _maxCapacity = options.EventOptions!.QueueCapacity;
         _webhookActionEventChannel = Channel.CreateBounded<WebhookActionEvent>(new BoundedChannelOptions(options.EventOptions!.QueueCapacity)
         {
             SingleWriter = true,
@@ -26,9 +31,9 @@ public class WebhookActionEventProcessor : IWebhookActionEventProcessor
 
     public async Task<List<WebhookActionEvent>> GetEvents()
     {
-        var events = new List<WebhookActionEvent>();
-        var webhookActionEvents = _webhookActionEventChannel.Reader.ReadAllAsync();
-        await foreach (var webhookActionEvent in webhookActionEvents) events.Add(webhookActionEvent);
-        return events;
+        await _webhookActionEventChannel.Reader.WaitToReadAsync();
+        var batch = new List<WebhookActionEvent>();
+        while (batch.Count < _maxCapacity && _webhookActionEventChannel.Reader.TryRead(out var actionEvent)) batch.Add(actionEvent);
+        return batch;
     }
 }
